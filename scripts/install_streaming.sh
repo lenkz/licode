3625eae8428bc3cba3068ee8cdf0dbdce2a66209
ratchakrit <ratchakrit@thevcgroup.com>
2013-12-24 16:04:43 +0700

    fix#0

--- scripts/install_streaming.sh
+++ scripts/install_streaming.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./licode/scripts/installUbuntuDeps_streaming.sh
 ./licode/scripts/installErizo.sh
-./licode/scripts/initLicode.sh
+./licode/scripts/initLicode_streaming.sh
 
 echo "installed streaming server but we also installed nuve module for emergency case(s)"
 echo 'please input "./licode/scripts/node.sh" to /etc/rc.local'
