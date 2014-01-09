440368444de664a75a698d62e5436eed4a450648
ratchakrit <ratchakrit@thevcgroup.com>
2014-01-09 15:13:00 +0700

    add pm2 to streaming

--- scripts/install_streaming.sh
+++ scripts/install_streaming.sh
@@ -3,5 +3,6 @@
 ./licode/scripts/installErizo.sh
 ./licode/scripts/initLicode_streaming.sh
 npm install pm2 -g
+/usr/bin/pm2 startup
 echo "installed streaming server but we also installed nuve module for emergency case(s)"
 echo 'please input "./licode/scripts/node.sh" to /etc/rc.local'
